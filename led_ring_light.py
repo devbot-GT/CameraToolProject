f9470bdfbfabbd17cbb2f9e3319af669b141fbf2
devbot-GT <139059054+devbot-GT@users.noreply.github.com>
2023-07-17 20:58:36 -0400

    Update led_ring_light.py

--- led_ring_light.py
+++ led_ring_light.py
@@ -6,32 +6,19 @@ Created on Tue Jul 11 13:46:59 2023
 @author: devbot
 """
 
-import RPi.GPIO as GPIO
+from gpiozero import PWMLED
 import sys
 
-# GPIO pin number for the LED ring light
-LED_PIN = 2
+# GPIO pin number for the PWM signal
+PWM_PIN = 12
 
-# Initialize the GPIO settings
-GPIO.setmode(GPIO.BCM)
-GPIO.setup(LED_PIN, GPIO.OUT)
-
-# Create a PWM object for controlling the LED brightness
-pwm_led = GPIO.PWM(LED_PIN, 300)  # Pin 2, frequency 300 Hz
-
-# Start the PWM with an initial duty cycle of 0 (LEDs off)
-pwm_led.start(0)
+# Create a PWMLED object for controlling the LED ring light
+pwm_led = PWMLED(PWM_PIN, frequency=300)
 
 # Function to set the brightness of the LED ring light
 def set_brightness(brightness):
-    # Map the brightness value (0-100) to the PWM duty cycle (0-100)
-    duty_cycle = (brightness / 100) * 100
-    pwm_led.ChangeDutyCycle(duty_cycle)
-
-# Function to clean up GPIO resources
-def cleanup_gpio():
-    pwm_led.stop()
-    GPIO.cleanup()
+    # Map the brightness value (0-100) to the PWM value (0-1)
+    pwm_led.value = brightness / 100
 
 # Main loop to continuously listen for brightness updates
 try:
@@ -41,7 +28,6 @@ try:
 
         if command == 'off':
             # Terminate the script
-            cleanup_gpio()
             break
 
         try:
@@ -51,5 +37,7 @@ try:
             pass
 
 except KeyboardInterrupt:
-    # Clean up GPIO resources when the script is interrupted
-    cleanup_gpio()
+    pass
+
+# Clean up resources
+pwm_led.close()
