d2cb1a805fc92b4db21b44c4a4cf6b1b56f90cc2
devbot-GT <139059054+devbot-GT@users.noreply.github.com>
2023-07-18 01:41:14 -0400

    Update led_ring_light.py

--- led_ring_light.py
+++ led_ring_light.py
@@ -6,48 +6,50 @@ Created on Tue Jul 11 13:46:59 2023
 @author: devbot
 """
 
-from gpiozero import PWMLED
-import RPi.GPIO as GPIO
+from gpiozero import PWMLED, DigitalOutputDevice
 import sys
-
-# Set up GPIO mode and warnings
-GPIO.setmode(GPIO.BCM)
-GPIO.setwarnings(False)
 
 # GPIO pin numbers
 POWER_PIN = 2
 PWM_PIN = 12
 
 # Set up GPIO pin 2 as an output
-GPIO.setup(POWER_PIN, GPIO.OUT)
+power = DigitalOutputDevice(POWER_PIN)
 
-# Set up PWM on GPIO pin 12 with a frequency of 300 Hz
-pwm_led = PWMLED(PWM_PIN, frequency=300)
+# Set up PWM on GPIO pin 12 with a frequency of 120 Hz
+pwm_led = PWMLED(PWM_PIN, frequency=120)
 
 # Function to set the brightness of the LED ring light
 def set_brightness(brightness):
     # Map the brightness value (0-100) to the PWM value (0-1)
     pwm_led.value = brightness / 100
 
-# Main loop to continuously listen for brightness updates
-try:
-    while True:
-        # Read the command from the SSH terminal
-        command = sys.stdin.readline().strip()
+def start_ring_light(brightness):
+    # Set the initial brightness of the LED and turn on
+    set_brightness(brightness)
+    # Main loop to continuously listen for brightness updates
+    try:
+        while True:
+            # Read the command from the SSH terminal
+            command = sys.stdin.readline().strip()
+    
+            if command == 'off':
+                # Terminate the script
+                break
+    
+            try:
+                brightness = int(command)
+                set_brightness(brightness)
+            except ValueError:
+                pass
+    
+    except KeyboardInterrupt:
+        pass
 
-        if command == 'off':
-            # Terminate the script
-            break
-
-        try:
-            brightness = int(command)
-            set_brightness(brightness)
-        except ValueError:
-            pass
-
-except KeyboardInterrupt:
-    pass
+# Get brightness from arg and illuminate the ring light
+brightness = int(sys.argv[1])
+start_ring_light(brightness)
 
 # Clean up resources
 pwm_led.close()
-GPIO.cleanup()
+power.close()
